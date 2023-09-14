5a06602b65e1b7565f36556a2961de0af875b306
Azazel test <Azazellampss@gmail.com>
2023-09-14 22:07:55 +0300

    1 commit

--- 7.3/jest/test/unit/sortByName.test.js
+++ 7.3/jest/test/unit/sortByName.test.js
@@ -2,16 +2,33 @@ const sorting = require("../../app");
 
 describe("Books names test suit", () => {
   it("Books names should be sorted in ascending order", () => {
-    expect(
-      sorting.sortByName([
-        "Гарри Поттер",
-        "Властелин Колец",
-        "Волшебник изумрудного города",
-      ])
-    ).toEqual([
+    const input = [
+      "Гарри Поттер",
+      "Властелин Колец",
+      "Волшебник изумрудного города",
+    ]
+    const expected = [
       "Властелин Колец",
       "Волшебник изумрудного города",
       "Гарри Поттер",
-    ]);
+    ]
+    const result = sorting.sortByName(input)
+
+    expect(result).toEqual(expected);
   });
+  test("Book titles should not be sorted", () => {
+    const input = [
+      "Гарри Поттер",
+      "Волшебник изумрудного города",
+      "Волшебник изумрудного города"
+    ];
+    const expected = [
+      "Волшебник изумрудного города",
+      "Волшебник изумрудного города",
+      "Гарри Поттер",
+    ];
+    const result = sorting.sortByName(input)
+    expect(result).toEqual(expected);
+  })
 });
+
